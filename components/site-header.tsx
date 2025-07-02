2dde1ab0848130cdae3bb07551a9e37884368790
Navaneeth Rameshwaram <rnavaneethkishore@gmail.com>
2025-07-01 23:55:03 -0700

    responsive navbar

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,41 +9,45 @@ import { ThemeToggle } from "@/components/theme-toggle"
 export function SiteHeader() {
   return (
     <header className="bg-background sticky top-0 z-40 w-full">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="container flex flex-col sm:flex-row h-auto sm:h-16 items-center sm:space-x-4 sm:justify-between sm:space-x-0 py-2 sm:py-0">
         <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end">
-          <nav className="flex items-center space-x-1 pr-4 md:pr-8 lg:pr-12">
-            <Link
-              href="https://www.linkedin.com/in/navaneethkr/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
+        <div className="w-full sm:w-auto flex flex-col sm:flex-row items-center">
+          <nav
+            className="flex w-full sm:w-auto flex-col sm:flex-row items-center justify-center sm:justify-end space-y-2 sm:space-y-0 sm:space-x-1 pt-2 sm:pt-0 pr-0 sm:pr-4 md:pr-8 lg:pr-12"
+          >
+            <div className="flex w-full justify-center sm:w-auto sm:justify-end">
+              <Link
+                href="https://www.linkedin.com/in/navaneethkr/"
+                target="_blank"
+                rel="noreferrer"
               >
-                <Icons.linkedin className="h-5 w-5" />
-                <span className="sr-only">LinkedIn</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
+                <div
+                  className={buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                  })}
+                >
+                  <Icons.linkedin className="h-5 w-5" />
+                  <span className="sr-only">LinkedIn</span>
+                </div>
+              </Link>
+              <Link
+                href={siteConfig.links.github}
+                target="_blank"
+                rel="noreferrer"
               >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <ThemeToggle />
+                <div
+                  className={buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                  })}
+                >
+                  <Icons.gitHub className="h-5 w-5" />
+                  <span className="sr-only">GitHub</span>
+                </div>
+              </Link>
+              <ThemeToggle />
+            </div>
           </nav>
         </div>
       </div>
